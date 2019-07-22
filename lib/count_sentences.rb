5ab0b04f1f85b94657eb98f9a5dc61887c08f943
Audrea Cook <audreacwork@gmail.com>
2019-07-22 21:05:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,16 @@ class String
   end
 
   def count_sentences
-    sentence_array = self.split(". " && "? " && "! ")
-    sentence_array.length
-    binding.pry
+    sentence_array = self.split()
+    end_punctuation_array = []
+    
+    sentence_array.each do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        end_punctuation_array << word
+      end
+    end
+    
+    end_punctuation_array.length
+    # binding.pry
   end
 end
