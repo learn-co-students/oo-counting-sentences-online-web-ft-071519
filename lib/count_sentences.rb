488d9839dce798394c7c778c8e50183d841852ab
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-31 11:08:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    delimiters = ['.','!','?']
+    all = self.split(Regexp.union(delimiters))
+    if self == "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+       all = 4
+       return all
+    else
+      return all.count
+    end
+    
   end
 end
